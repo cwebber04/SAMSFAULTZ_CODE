5555439cd07a5764bfada100dd9733963f388ef1
TimothyTLee <webber04@gmail.com>
2016-06-22 19:03:38 +0200

    22.06.2016 "py_seismicity_schweiz.py": add save image function

--- py_seismicity_schweiz.py
+++ py_seismicity_schweiz.py
@@ -54,45 +54,49 @@ for x in range(0, evtnum):
 	hypodep.append(event.origins[0].depth/1000)
 	eqmag.append(event.magnitudes[0].mag)
 
-##---MAPPING_DATA---#
-##global map
-#global_map = Basemap(projection='robin', lat_0=7.5, lon_0=47.5, resolution='l', area_thresh=1000)#, llcrnrlon=-136.25, llcrnrlat=56, urcrnrlon=-134.25, urcrnrlat=57.75) #also try: projection='ortho'
-#global_map.drawcoastlines()
-#global_map.drawcountries()
-#global_map.fillcontinents(color='KHAKI') #try also: global_map.bluemarble()
-#global_map.drawmapboundary()
-#global_map.drawmeridians(np.arange(0, 360, 30))
-#global_map.drawparallels(np.arange(-90, 90, 30))
-#lons = [8.547219]
-#lats = [47.378578] 
-#x,y = global_map(lons, lats)
-#global_map.plot(x, y, marker='o', markerfacecolor='turquoise', markersize=12) #or use abbr. like: 'bo' -means blue circle and use different properties like: , color='b', linestyle='dashed'
-#labels = ['SED']
-#x_offsets = [10000]
-#y_offsets = [5000]
-#for label, xpt, ypt, x_offset, y_offset in zip(labels, x, y, x_offsets, y_offsets):
-#	plt.text(xpt+x_offset, ypt+y_offset, label)
-#plt.show()
-#
-##regional map
-#regional_map = Basemap(projection='merc', lat_0=7.5, lon_0=47.5, resolution='h', area_thresh=0.1, llcrnrlon=5, urcrnrlon=13, llcrnrlat=45.5, urcrnrlat=49) #also try: projection='ortho'
-#regional_map.drawcoastlines()
-#regional_map.drawcountries()
-#regional_map.fillcontinents(color='KHAKI')
-#regional_map.drawmapboundary()
-#regional_map.drawmeridians(np.arange(0, 360, 30))
-#regional_map.drawparallels(np.arange(-90, 90, 30))
-#lons = [08.547219]
-#lats = [47.378578] 
-#x,y = regional_map(lons, lats)
-#regional_map.plot(x, y, marker='o', markerfacecolor='turquoise', markersize=12) #or use abbr. like: 'bo' -means blue circle and use different properties like: , color='b', linestyle='dashed'
-#labels = ['SED']
-#x_offsets = [10000]
-#y_offsets = [5000]
-#for label, xpt, ypt, x_offset, y_offset in zip(labels, x, y, x_offsets, y_offsets):
-#	plt.text(xpt+x_offset, ypt+y_offset, label)
-#plt.show()
-#
+#---MAPPING_DATA---#
+#global map
+global_map = Basemap(projection='robin', lat_0=7.5, lon_0=47.5, resolution='l', area_thresh=1000)#, llcrnrlon=-136.25, llcrnrlat=56, urcrnrlon=-134.25, urcrnrlat=57.75) #also try: projection='ortho'
+global_map.drawcoastlines()
+global_map.drawcountries()
+global_map.fillcontinents(color='KHAKI') #try also: global_map.bluemarble()
+global_map.drawmapboundary()
+global_map.drawmeridians(np.arange(0, 360, 30))
+global_map.drawparallels(np.arange(-90, 90, 30))
+lons = [8.547219]
+lats = [47.378578] 
+x,y = global_map(lons, lats)
+global_map.plot(x, y, marker='o', markerfacecolor='turquoise', markersize=12) #or use abbr. like: 'bo' -means blue circle and use different properties like: , color='b', linestyle='dashed'
+labels = ['SED']
+x_offsets = [10000]
+y_offsets = [5000]
+for label, xpt, ypt, x_offset, y_offset in zip(labels, x, y, x_offsets, y_offsets):
+	plt.text(xpt+x_offset, ypt+y_offset, label)
+plt.plot
+plt.savefig('py_seismicity_schweiz_global.png')
+plt.show()
+
+#regional map
+regional_map = Basemap(projection='merc', lat_0=7.5, lon_0=47.5, resolution='h', area_thresh=0.1, llcrnrlon=5, urcrnrlon=13, llcrnrlat=45.5, urcrnrlat=49) #also try: projection='ortho'
+regional_map.drawcoastlines()
+regional_map.drawcountries()
+regional_map.fillcontinents(color='KHAKI')
+regional_map.drawmapboundary()
+regional_map.drawmeridians(np.arange(0, 360, 30))
+regional_map.drawparallels(np.arange(-90, 90, 30))
+lons = [08.547219]
+lats = [47.378578] 
+x,y = regional_map(lons, lats)
+regional_map.plot(x, y, marker='o', markerfacecolor='turquoise', markersize=12) #or use abbr. like: 'bo' -means blue circle and use different properties like: , color='b', linestyle='dashed'
+labels = ['SED']
+x_offsets = [10000]
+y_offsets = [5000]
+for label, xpt, ypt, x_offset, y_offset in zip(labels, x, y, x_offsets, y_offsets):
+	plt.text(xpt+x_offset, ypt+y_offset, label)
+plt.plot
+plt.savefig('py_seismicity_schweiz_regional.png')
+plt.show()
+
 #hypocenter map
 eq_map = Basemap(projection='merc', lat_0=7.5, lon_0=47.5, resolution='h', area_thresh=0.1, llcrnrlon=5, urcrnrlon=13, llcrnrlat=45.5, urcrnrlat=49) #also try: projection='ortho'
 eq_map.drawcoastlines()
@@ -116,28 +120,36 @@ for label, xpt, ypt in zip(labels, xpts, ypts):
 title_string = "Earthquakes of Magnitude 1.0 or Greater\n" #title
 title_string += "%s through %s" % (starttime, endtime)
 plt.title(title_string)
+plt.plot
+plt.savefig('py_seismicity_schweiz_hypo.png')
 plt.show()
 
 #---HISTOGRAM---#
-##time series
-#inter_event_times = []
-#for i in range(1, len(focaltime)):
-#    dt = UTCDateTime(focaltime[i-1]) - UTCDateTime(focaltime[len(focaltime)-1])
-#    dt = dt / 86400
-#    inter_event_times.append(dt)
-#plt.hist(inter_event_times)#, bins=range(0, 200, 10))
-#plt.xlabel("Magnitude 1+ interevent times since 2016 [days]")
-#plt.ylabel("count")
-#plt.show()
+#time series
+inter_event_times = []
+for i in range(1, len(focaltime)):
+    dt = UTCDateTime(focaltime[i-1]) - UTCDateTime(focaltime[len(focaltime)-1])
+    dt = dt / 86400
+    inter_event_times.append(dt)
+plt.hist(inter_event_times)#, bins=range(0, 200, 10))
+plt.xlabel("Magnitude 1+ event since 2016 [days]")
+plt.ylabel("count")
+plt.plot
+plt.savefig('py_seismicity_schweiz_histo_time.png')
+plt.show()
 
 #depth series
-#plt.hist(hypodep)#, bins=range(0, 200, 10))
-#plt.xlabel("Depth since 2016 [km]")
-#plt.ylabel("count")
-#plt.show()
+plt.hist(hypodep)#, bins=range(0, 200, 10))
+plt.xlabel("Depth since 2016 [km]")
+plt.ylabel("count")
+plt.plot
+plt.savefig('py_seismicity_schweiz_histo_depth.png')
+plt.show()
 
 #magnitude series
-#plt.hist(eqmag)#, bins=range(0, 200, 10))
-#plt.xlabel("Mag. since 2016 [Ml]")
-#plt.ylabel("count")
-#plt.show()
+plt.hist(eqmag)#, bins=range(0, 200, 10))
+plt.xlabel("Mag. since 2016 [Ml]")
+plt.ylabel("count")
+plt.plot
+plt.savefig('py_seismicity_schweiz_histo_mag.png')
+plt.show()
